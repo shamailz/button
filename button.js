031553e0c5aba7380be9094a290a1419e113e015
Shamail <zahis538@newschool.edu>
2019-05-08 15:57:09 -0400

    Initial commit

---
+++ button.js
@@ -0,0 +1,12 @@
+
+
+
+function image(){
+
+		var whatever = document.createElement("img")
+		whatever.className = "picture" 
+
+		whatever.src = "images/sparkle.png"
+		document.getElementById("wrapper").appendChild(whatever)
+
+}
